01251fbdc4559839325969093753371513f5f86d
SakthiAishwaryaS <116715772+SakthiAishwaryaS@users.noreply.github.com>
2025-04-19 19:48:24 +0000

    IACAI-1 Fix login bug

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const commitMessages = [
     if (match) {
       console.log(`✅ Jira Key(s) found in: "${msg}" → ${match.join(", ")}`);
     } else {
-      console.log(`❌ No Jira Key found in: "${msg}"`);
+      console.log(`❌ No Jir Key found in: "${msg}"`);
     }
   });
   
